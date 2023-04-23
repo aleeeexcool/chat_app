65333a5349bd8fb0ca0ac14f206565ad735dfd4f
aleeeexcool <ramzhev.sasha@outlook.com>
2023-04-23 20:24:17 +0300

    project done

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-    return <>chats....</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    'fde8cee9-61b6-403c-a06c-2d84d6b4d80e', 
+    props.user.username, 
+    props.user.secret
+  );
+  return (
+    <div style={{ height: '100vh' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+    </div>
+    )
 }
 
 export default ChatsPage;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -12,7 +12,7 @@ app.post("/authenticate", async (req, res) => {
     const r = await axios.put(
       "https://api.chatengine.io/users/",
       { username: username, secret: username, first_name: username },
-      { headers: { "private_ket": "9f73d7c8-f877-4833-af0e-eadee5322d3e" } }
+      { headers: { "private_ket": "fd9e3d00-0f50-4faa-bf58-9a5ce88a6e9d" } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
